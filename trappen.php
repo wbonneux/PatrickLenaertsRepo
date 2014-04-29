ab6f8f9f059cf1069d1bde3a0a4fa6284f59e17c
wbonneux <wbonneux@gmail.com>
2014-04-29 10:45:44 +0200

    Aanpassing voor realistate & voor & na foto's

--- trappen.php
+++ trappen.php
@@ -1,4 +1,4 @@
-	<?php include '_/components/header_include.php';?>
+ 	<?php include '_/components/header_include.php';?>
 <!-- /Header -->
 <!-- START CONTENT -->
 <body>
@@ -26,6 +26,8 @@
 						<li><a href="javascript:void(0);" id="ztrap">Z-trappen</a></li>
 
 						<li><a href="javascript:void(0);" id="rtrap">Rechte trappen</a></li>
+						
+						<li><a href="javascript:void(0);" id="voornatrap">Voor & Na</a></li>
 
 					</ul>
 				</div>
@@ -37,12 +39,52 @@
 
 			<div class="portfolio_1_2 myportfolio_1_2">
 				<ul id="list">
-					<!-- START PORTFOLIO COLUMN #1 -->
-					<li id="id-1" class="ztrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_6.JPG"
+				
+				<!-- START PORTFOLIO COLUMN #8 -->
+					<li id="id-11" class="voornatrap"><span class="image"> <a
+							href="images/realisaties/trappen/Before_After_1.jpg"
 							data-rel="prettyPhoto" class="img-thumb"><img
 								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_6.JPG"
+								src="images/realisaties/trappen/Before_After_1.jpg"
+								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
+					</span> <!-- 					<span class="description"> -->
+						<!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
+						<!-- 					</span> --> <span class="clear padding15"></span></li>
+					<!-- END PORTFOLIO COLUMN #8 -->
+					
+				<!-- START PORTFOLIO COLUMN #8 -->
+					<li id="id-9" class="voornatrap"><span class="image"> <a
+							href="images/realisaties/trappen/Before_After_3.jpg"
+							data-rel="prettyPhoto" class="img-thumb"><img
+								class="portfolio_image"
+								src="images/realisaties/trappen/Before_After_3.jpg"
+								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
+					</span> <!-- 					<span class="description"> -->
+						<!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
+						<!-- 					</span> --> <span class="clear padding15"></span></li>
+					<!-- END PORTFOLIO COLUMN #8 -->
+					
+					<!-- START PORTFOLIO COLUMN #8 -->
+					<li id="id-10" class="voornatrap"><span class="image"> <a
+							href="images/realisaties/trappen/Before_After_2.jpg"
+							data-rel="prettyPhoto" class="img-thumb"><img
+								class="portfolio_image"
+								src="images/realisaties/trappen/Before_After_2.jpg"
+								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
+					</span> <!-- 					<span class="description"> -->
+						<!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
+						<!-- 					</span> --> <span class="clear padding15"></span></li>
+					<!-- END PORTFOLIO COLUMN #8 -->
+					
+					
+					
+					
+					<!-- START PORTFOLIO COLUMN #1 -->
+					<li id="id-1" class="rtrap"><span class="image"> <a
+							href="images/realisaties/trappen/Realisatie_1.JPG"
+							data-rel="prettyPhoto" class="img-thumb"><img
+								class="portfolio_image"
+								src="images/realisaties/trappen/Realisatie_1.JPG"
 								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
 					</span> 
 						<!-- 					<span class="description"> --> <!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
@@ -51,10 +93,10 @@
 
 					<!-- START PORTFOLIO COLUMN #2 -->
 					<li id="id-2" class="rtrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_1.JPG"
+							href="images/realisaties/trappen/Realisatie_2.JPG"
 							data-rel="prettyPhoto" class="img-thumb"><img
 								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_1.JPG"
+								src="images/realisaties/trappen/Realisatie_2.JPG"
 								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
 					</span> <!-- 					<span class="description"> -->
 						<!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
@@ -62,11 +104,11 @@
 					<!-- END PORTFOLIO COLUMN #2 -->
 
 					<!-- START PORTFOLIO COLUMN #3 -->
-					<li id="id-3" class="rtrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_2.JPG"
+					<li id="id-3" class="ztrap"><span class="image"> <a
+							href="images/realisaties/trappen/Realisatie_3.JPG"
 							data-rel="prettyPhoto" class="img-thumb"><img
 								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_2.JPG"
+								src="images/realisaties/trappen/Realisatie_3.JPG"
 								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
 					</span> 
 						<!-- 					<span class="description"> --> <!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
@@ -75,10 +117,10 @@
 
 					<!-- START PORTFOLIO COLUMN #4 -->
 					<li id="id-4" class="rtrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_3.JPG"
+							href="images/realisaties/trappen/Realisatie_4.JPG"
 							data-rel="prettyPhoto" class="img-thumb"><img
 								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_3.JPG"
+								src="images/realisaties/trappen/Realisatie_4.JPG"
 								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
 					</span> <!-- 					<span class="description"> -->
 						<!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
@@ -87,10 +129,10 @@
 
 					<!-- START PORTFOLIO COLUMN #5 -->
 					<li id="id-5" class="rtrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_4.JPG"
+							href="images/realisaties/trappen/Realisatie_5.JPG"
 							data-rel="prettyPhoto" class="img-thumb"><img
 								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_4.JPG"
+								src="images/realisaties/trappen/Realisatie_5.JPG"
 								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
 					</span>  <!-- 					<span class="description"> --> <!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
 						<!-- 					</span> --> <span class="clear padding15"></span></li>
@@ -98,10 +140,10 @@
 
 					<!-- START PORTFOLIO COLUMN #6 -->
 					<li id="id-6" class="rtrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_5.JPG"
+							href="images/realisaties/trappen/Realisatie_6.JPG"
 							data-rel="prettyPhoto" class="img-thumb"><img
 								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_5.JPG"
+								src="images/realisaties/trappen/Realisatie_6.JPG"
 								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
 					</span>  <!-- 					<span class="description"> -->
 						<!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
@@ -109,11 +151,11 @@
 					<!-- END PORTFOLIO COLUMN #6 -->
 
 					<!-- START PORTFOLIO COLUMN #7 -->
-					<li id="id-7" class="ztrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_7.JPG"
+					<li id="id-7" class="rtrap"><span class="image"> <a
+							href="images/realisaties/trappen/Realisatie_7.JPG"
 							data-rel="prettyPhoto" class="img-thumb"><img
 								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_7.JPG"
+								src="images/realisaties/trappen/Realisatie_7.JPG"
 								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
 					</span><!-- 					<span class="description"> --> <!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
 						<!-- 					</span> --> <span class="clear padding15"></span></li>
@@ -121,27 +163,18 @@
 
 					<!-- START PORTFOLIO COLUMN #8 -->
 					<li id="id-8" class="rtrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_8.JPG"
+							href="images/realisaties/trappen/Realisatie_8.JPG"
 							data-rel="prettyPhoto" class="img-thumb"><img
 								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_8.JPG"
+								src="images/realisaties/trappen/Realisatie_8.JPG"
 								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
 					</span> <!-- 					<span class="description"> -->
 						<!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
 						<!-- 					</span> --> <span class="clear padding15"></span></li>
 					<!-- END PORTFOLIO COLUMN #8 -->
+					
+					
 
-					<!-- START PORTFOLIO COLUMN #9 -->
-					<li id="id-9" class="rtrap"><span class="image"> <a
-							href="images/realisaties/trappen/lightbox/trappen_PatrickLenaerts_9.JPG"
-							data-rel="prettyPhoto" class="img-thumb"><img
-								class="portfolio_image"
-								src="images/realisaties/trappen/trappen_PatrickLenaerts_9.JPG"
-								alt="Patrick Lenaerts, specialist in het bekleden van betonnen trappen in hout" /></a>
-					</span> 
-						<!-- 					<span class="description"> --> <!-- 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
-						<!-- 					</span> --> <span class="clear padding15"></span></li>
-					<!-- END PORTFOLIO COLUMN #9 -->
 
 				</ul>
 			</div>
